f935548167e954a35d49cea788377d5a7b41b0d3
marcelle alves <marcellealves.dev@gmail.com>
2026-06-11 18:47:07 -0300

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,15 +3,18 @@ import database from 'infra/database.js'
 async function status(request, response) {
   const updatedAt = new Date().toISOString()
 
+  // Server version
   const databaseVersionResult = await database.query('SHOW server_version;')
   const databaseVersionValue = databaseVersionResult.rows[0].server_version
 
+  // Max connections
   const databaseMaxConnectionsResult = await database.query(
     'SHOW max_connections;'
   )
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections
 
+  // Opened connections
   const databaseName = process.env.POSTGRES_DB
   const databaseOpenedConnectionsResult = await database.query({
     text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
