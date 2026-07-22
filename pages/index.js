2f6af4c31916dd31f8a44dde42c43c06d7201620
marcelle alves <marcellealves.dev@gmail.com>
2026-07-22 16:04:32 -0300

    fix `eslint` linting

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <h1>
-      The mystery of life isn't a problem to solve, but a reality to experience.
+      {
+        "The mystery of life isn't a problem to solve, but a reality to experience."
+      }
     </h1>
   )
 }
