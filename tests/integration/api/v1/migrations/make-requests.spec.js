79333aaba2ad5709fc59bfb03504880493f1c38d
marcelle alves <marcellealves.dev@gmail.com>
2026-08-10 18:10:48 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/make-requests.spec.js
+++ tests/integration/api/v1/migrations/make-requests.spec.js
@@ -6,33 +6,43 @@ beforeAll(async () => {
   await database.query('drop schema public cascade; create schema public')
 })
 
-test('Methods that are not "GET" or "POST" to /api/v1/migrations should return 405', async () => {
-  const deleteResponse = await fetch(
-    'http://localhost:3000/api/v1/migrations',
-    {
-      method: 'DELETE'
-    }
-  )
+describe('DELETE || PUT || PATCH /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    test('Running pending migrations with different methods', async () => {
+      const deleteResponse = await fetch(
+        'http://localhost:3000/api/v1/migrations',
+        {
+          method: 'DELETE'
+        }
+      )
 
-  const putResponse = await fetch('http://localhost:3000/api/v1/migrations', {
-    method: 'PUT'
+      const putResponse = await fetch(
+        'http://localhost:3000/api/v1/migrations',
+        {
+          method: 'PUT'
+        }
+      )
+
+      const patchResponse = await fetch(
+        'http://localhost:3000/api/v1/migrations',
+        {
+          method: 'PATCH'
+        }
+      )
+
+      const databaseName = process.env.POSTGRES_DB
+      const databaseOpenedConnectionsResult = await database.query({
+        text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
+        values: [databaseName]
+      })
+      const databaseOpenedConnectionsValue =
+        databaseOpenedConnectionsResult.rows[0].count
+
+      expect(databaseOpenedConnectionsValue).toBe(1)
+
+      expect(deleteResponse.status).toBe(405)
+      expect(putResponse.status).toBe(405)
+      expect(patchResponse.status).toBe(405)
+    })
   })
-
-  const patchResponse = await fetch('http://localhost:3000/api/v1/migrations', {
-    method: 'PATCH'
-  })
-
-  const databaseName = process.env.POSTGRES_DB
-  const databaseOpenedConnectionsResult = await database.query({
-    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
-    values: [databaseName]
-  })
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count
-
-  expect(databaseOpenedConnectionsValue).toBe(1)
-
-  expect(deleteResponse.status).toBe(405)
-  expect(putResponse.status).toBe(405)
-  expect(patchResponse.status).toBe(405)
 })
--- tests/integration/api/v1/migrations/post.spec.js
+++ tests/integration/api/v1/migrations/post.spec.js
@@ -6,39 +6,47 @@ beforeAll(async () => {
   await database.query('drop schema public cascade; create schema public')
 })
 
-test('POST to /api/v1/migrations should return 201', async () => {
-  const firstPostResponse = await fetch(
-    'http://localhost:3000/api/v1/migrations',
-    {
-      method: 'POST'
-    }
-  )
-  const secondPostResponse = await fetch(
-    'http://localhost:3000/api/v1/migrations',
-    {
-      method: 'POST'
-    }
-  )
+describe('POST /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    describe('Running pending migrations', () => {
+      test('For the first time', async () => {
+        const firstPostResponse = await fetch(
+          'http://localhost:3000/api/v1/migrations',
+          {
+            method: 'POST'
+          }
+        )
 
-  const firstResponseBody = await firstPostResponse.json()
-  const secondResponseBody = await secondPostResponse.json()
+        const firstResponseBody = await firstPostResponse.json()
 
-  const databaseName = process.env.POSTGRES_DB
-  const databaseOpenedConnectionsResult = await database.query({
-    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
-    values: [databaseName]
+        expect(firstPostResponse.status).toBe(201)
+        expect(Array.isArray(firstResponseBody)).toBe(true)
+        expect(firstResponseBody.length).toBeGreaterThan(0)
+      })
+
+      test('For the second time', async () => {
+        const secondPostResponse = await fetch(
+          'http://localhost:3000/api/v1/migrations',
+          {
+            method: 'POST'
+          }
+        )
+
+        const secondResponseBody = await secondPostResponse.json()
+
+        const databaseName = process.env.POSTGRES_DB
+        const databaseOpenedConnectionsResult = await database.query({
+          text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
+          values: [databaseName]
+        })
+        const databaseOpenedConnectionsValue =
+          databaseOpenedConnectionsResult.rows[0].count
+
+        expect(secondPostResponse.status).toBe(200)
+        expect(Array.isArray(secondResponseBody)).toBe(true)
+        expect(secondResponseBody.length).toEqual(0)
+        expect(databaseOpenedConnectionsValue).toEqual(1)
+      })
+    })
   })
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count
-
-  expect(firstPostResponse.status).toBe(201)
-  expect(secondPostResponse.status).toBe(200)
-
-  expect(Array.isArray(firstResponseBody)).toBe(true)
-  expect(firstResponseBody.length).toBeGreaterThan(0)
-
-  expect(Array.isArray(secondResponseBody)).toBe(true)
-  expect(secondResponseBody.length).toEqual(0)
-
-  expect(databaseOpenedConnectionsValue).toEqual(1)
 })
